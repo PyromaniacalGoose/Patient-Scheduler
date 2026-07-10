d8937b42fdf7a4d34a1a143e0d38a4fe9196318c
Mathias Johnbeck <bamj@itu.dk>
2026-07-10 19:07:17 +0200

    Domain side models for scheduler logic

---
+++ scheduling/models.py
@@ -0,0 +1,60 @@
+"""
+Dataclasses for domain level model representation.
+patiant info isn't needed in scheduling logic, so we don't expose it domain side.
+OBS it is important to keep this along with the repositories translating between it and the django ORM in-sync
+"""
+from dataclasses import dataclass, replace
+from datetime import datetime
+from enum import IntEnum
+
+class AppointmentStatus(IntEnum):
+    SCHEDULED = 1
+    FINISHED = 2
+    CANCELLED = 3
+    NO_SHOW = 4
+
+class CourseStatus(IntEnum):
+    PLANNED = 1
+    ACTIVE = 2
+    COMPLETED = 3
+    CANCELLED = 4
+
+class TreatmentType(IntEnum):
+    V1 = 1
+    V2 = 2
+
+@dataclass(frozen=True)
+class Appointment:
+    id: int | None
+    course_id: int
+    slot_id: int
+    treatment_number: int
+    status: AppointmentStatus
+    type: TreatmentType
+    note: str
+    
+    def __post_init__(self):
+        if self.treatment_number <= 1:
+            raise ValueError("treatment_number must be above 0")
+
+@dataclass(frozen=True)
+class TreatmentSlot:
+    id: int | None
+    space_id: int
+    start_time: datetime
+    end_time: datetime
+
+    def __post_init__(self):
+        if self.end_time <= self.start_time:
+            raise ValueError("end_time must be after start_time")
+
+@dataclass(frozen=True)
+class TreatmentCourse:
+    id: int | None
+    patient_id: int
+    planned_treatments: int
+    status: CourseStatus
+        
+    def __post_init__(self):
+        if self.planned_treatments <= 0:
+            raise ValueError("planned_treatments cannot be negative")
